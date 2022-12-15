caab81c7d33deb40eb4c65d01677af6aa58c37c1
Wojciech Zajkowski <traumatictiltexperience@gmail.com>
2022-12-15 13:59:51 +0900

    loo/waic now can intake 'CmdStanMCMC_CSV' class

--- R/stan_model_comp.R
+++ R/stan_model_comp.R
@@ -7,14 +7,17 @@
 #'
 #' @examples
 calc_loo <- function(fit) {
-  if (class(fit)[1] == 'character') fit = targets::tar_read_raw(fit) #if input is a character, read it out
+  if (class(fit)[1] == 'character')       fit = targets::tar_read_raw(fit) #if input is a character, read it out
+  if (class(fit)[1] == 'CmdStanMCMC_CSV') { # reading chain-based csv files case
+    draws = fit$draws()
+    fit = apply(draws, c(1,3), mean) %>% as_tibble() #average across chains
+  }
   loglik = fit %>%
-    select(starts_with('log_lik')) %>%
+    select(contains('log_lik')) %>%
     select(where(~!any(is.na(.))))
   out = loo::loo.array(posterior::as_draws_array(loglik))
   return(out)
 }
-
 #' Calculate WAIC (and llpd) of a cmdstan model object
 #'
 #' @param fit either draws or a character vector which can be retrieved using targets
@@ -25,8 +28,12 @@ calc_loo <- function(fit) {
 #' @examples
 calc_waic<-function(fit) {
   if (class(fit)[1] == 'character') fit = targets::tar_read_raw(fit) #if input is a character, read it out
+  if (class(fit)[1] == 'CmdStanMCMC_CSV') { # reading chain-based csv files case
+    draws = fit$draws()
+    fit = apply(draws, c(1,3), mean) %>% as_tibble() #average across chains
+  }
   loglik = fit %>%
-    select(starts_with('log_lik')) %>%
+    select(contains('log_lik')) %>%
     select(where(~!any(is.na(.))))
   out = LaplacesDemon::WAIC(t(loglik))
   return(out)
